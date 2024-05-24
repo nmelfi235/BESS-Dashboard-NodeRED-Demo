917db4e5538ad740ee2a09d1666bfb63fadc3382
Nick Melfi <nmelfi235@gmail.com>
2024-05-24 15:40:51 -0400

    Created a line graph for Battery Voltage and Current

--- BESS-Dashboard-Demo/src/App.tsx
+++ BESS-Dashboard-Demo/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import BatteryVoltageCurrentGraph from "./BatteryVoltageCurrentGraph";
 import CardArea from "./CardArea";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <h1>BESS Dashboard Clone</h1>
       <CardArea />
+      <BatteryVoltageCurrentGraph />
     </>
   );
 }
